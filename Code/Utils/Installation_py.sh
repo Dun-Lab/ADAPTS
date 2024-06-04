c98a53a1343c321a2a1dd487d53c12245b436b74
uqer-jyh <yuanhao.jiang@uqconnect.edu.au>
2024-06-04 16:22:35 +1000

    tidy up the code

---
+++ Code/Utils/Installation_py.sh
@@ -0,0 +1,10 @@
+pip install scanpy
+pip install decoupler
+pip install leidenalg
+pip install igraph
+pip install omnipath
+pip install liana
+pip install decoupler
+
+
+# pip install gseapy==1.0.6
--- Utils/Installation_py.sh
+++
@@ -1,6 +0,0 @@
-pip install scanpy
-pip install decoupler
-pip install leidenalg
-pip install igraph
-pip install omnipath
-pip install gseapy==1.0.6
